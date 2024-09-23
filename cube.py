e5cf5c059675d61cb01d4e51c70de839efad1267
alejandro <alejandro@astrafy.io>
2024-09-23 14:55:11 +0000

    Merge alejandro/test to master

--- cube.py
+++ cube.py
@@ -1,3 +1,16 @@
 # Cube configuration options: https://cube.dev/docs/config
 
 from cube import config
+
+@config('driver_factory')
+def driver_factory(ctx: dict) -> None:
+  context = ctx['securityContext']
+  data_source = ctx['dataSource']
+ 
+  return {
+    'type': 'postgres',
+    'host': 'demo-db-examples.cube.dev',
+    'user': 'cube',
+    'password': '12345',
+    'database': data_source
+  }
